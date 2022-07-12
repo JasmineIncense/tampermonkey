508d65d2f35ab4b9b706577deefe487c3b25e083
Jasmine <jasminechoi@qq.com>
2022-07-12 23:52:34 +0800

    axio拉取接口

--- src/tamper-pocket.js
+++ src/tamper-pocket.js
@@ -4,13 +4,32 @@
 // @version      0.1
 // @description  pocket comic漫画网 tamper油猴脚本 上下翻页阅读器爬虫
 // @author       zero_degree@foxmail.com
-// @match        https://www.pocketcomics.com/comic/*/chapter/*
+// @match        https://www.pocketcomics.com/comic*/chapter*/product
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pocketcomics.com
 // @grant        none
+
+// @require           https://unpkg.com/axios/dist/axios.min.js
 // ==/UserScript==
 
 // 文件命名计数
 let count = 1
+const down = async(imgUrl) => {
+  const fileStream = await axios.get(imgUrl, {
+    responseType: 'blob',
+    withCredentials: false
+  })
+  if (!fileStream) {
+    return false;
+  }
+  const url = URL.createObjectURL(fileStream.data);
+  const aLink = document.createElement('a');
+  aLink.href = url;
+  aLink.setAttribute('download',count);
+  aLink.click();
+  URL.revokeObjectURL(url);
+
+  count++
+}
 const run = () => {
   setTimeout(() => {
     const nodes = document.getElementsByClassName('wrap-canvas')
@@ -18,16 +37,9 @@ const run = () => {
     Array.prototype.map.call(nodes, (item) => {
       const style = item.currentStyle || window.getComputedStyle(item, false)
       const imgUrl = style.backgroundImage.slice(4, -1).replace(/"/g, "")
-      console.log(imgUrl);
-
-      const download = document.createElement('a')
-      download.setAttribute('download', count)
-      download.href = imgUrl
-      download.click()
-      count++
+      down(imgUrl)
     })
-    run()
-  }, 10000)
+  }, 12000)
 }
 
 (function() {
